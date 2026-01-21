bd7cc8394f7a54b1d1b00b05bcb927f37a3c046d
suhaasvj <suhaasvj@gmail.com>
2026-01-21 23:01:20 +0530

    a

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
